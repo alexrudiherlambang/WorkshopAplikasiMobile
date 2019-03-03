e0bc457c317622ed81e325f53495dec49d46cfc2
Alex Rudi Herlambang <alexrudiherlambang98@gmail.com>
2019-03-03 18:55:20 +0700

    cek

---
+++ HelloConstraint/app/src/main/java/com/example/helloconstraint/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.helloconstraint;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+    @SuppressLint("SetText 18n")
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+    public void showToast(View view){
+        Toast toast = Toast.makeText(this,R.string.toast_button_toast, Toast.LENGTH_LONG);
+        toast.show();
+    }
+}
