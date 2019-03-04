a735fd3886b287ae1991ba15eda27244384b2514
Alex Rudi Herlambang <alexrudiherlambang98@gmail.com>
2019-03-04 16:14:18 +0700

    counter

--- HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
+++ HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.hellotoast;
 import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private int mCount = 0;
     private TextView mShowCount;
+    private  static String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +24,16 @@ public class MainActivity extends AppCompatActivity {
         mCount++;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
+            if (mCount % 10 == 0){
+                Log.d(LOG_TAG, "Perhitungan adalah kelipatan: " + mCount );
+            }
     }
     public void showToast(View view){
         Toast toast = Toast.makeText(this,R.string.toast_button_toast, Toast.LENGTH_LONG);
         toast.show();
+    }
+    public void resetCount(View view){
+        mShowCount.setText(Integer.toString(0));
+        mCount = 0;
     }
 }
